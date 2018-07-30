326d0cec5677c6846b5dbfb5c4d37ccdf583217d
Lahiru Kaushalya Ananda <lahiruk.14@cse.mrt.ac.lk>
2018-07-30 13:40:35 +0530

    Create vocabulary from text file

--- CS4642_TF-IDF Questions/Program.cs
+++ CS4642_TF-IDF Questions/Program.cs
@@ -1,17 +1,37 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace CS4642_TF_IDF_Questions
+namespace TF_IDF
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Read Input files
+            var file1 = File.ReadAllText("../../Files/document1.txt");
+            var file2 = File.ReadAllText("../../Files/document2.txt");
+            var file3 = File.ReadAllText("../../Files/document3.txt");
+
+            //Create Vocabularies
+            Vocabulary vocabulary = new Vocabulary();
+
+            var vocabulary1 = vocabulary.CreateVocabulary(file1);
+            var vocabulary2 = vocabulary.CreateVocabulary(file2);
+            var vocabulary3 = vocabulary.CreateVocabulary(file3);
+
+            PrintVocabulary(vocabulary3);
+
             Console.ReadKey();
         }
+
+        private static void PrintVocabulary(SortedDictionary<string,int> vocabulary)
+        {
+            foreach (KeyValuePair<string, int> kvp in vocabulary)
+            {
+                Console.WriteLine("{0} : {1}", kvp.Key, kvp.Value);
+            }
+        }
+
     }
 }
---
+++ CS4642_TF-IDF Questions/Vocabulary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TF_IDF
+{
+    public class Vocabulary
+    {
+        private static Dictionary<string, string> WordsDic;
+
+        public SortedDictionary<string, int> CreateVocabulary(string fileString)
+        {
+            //Get words dictionary json file 
+            var text = File.ReadAllText("../../Files/words_dictionary.json");
+            WordsDic = JsonConvert.DeserializeObject<Dictionary<string, string>>(text);
+
+            var temp = FilterLetters(fileString);
+            var vocabulary = GetVocabulary(temp);
+
+            return vocabulary;
+        }
+
+        private static SortedDictionary<string, int> GetVocabulary(string[] words)
+        {
+            var dic = new SortedDictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (WordsDic.ContainsKey(word))
+                {
+                    if (dic.ContainsKey(word))
+                    {
+                        var count = dic[word];
+                        dic[word] = count + 1;
+                    }
+                    else
+                    {
+                        dic[word] = 1;
+                    }
+                }
+            }
+            return dic;
+        }
+
+        private static string[] FilterLetters(string fileString)
+        {
+            ;
+            foreach (char _char in fileString)
+            {
+                if (!Char.IsLetter(_char) && _char != ' ')
+                {
+                    fileString = fileString.Replace(_char, ' ');
+                }
+            }
+            return fileString.Split(' ');
+        }
+
+    }
+}
