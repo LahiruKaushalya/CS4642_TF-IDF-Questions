2baeefdda905b8433b7aab75faa304a84c1e205f
Lahiru Kaushalya Ananda <lahiruk.14@cse.mrt.ac.lk>
2018-07-30 13:34:51 +0530

    Add project files.

---
+++ CS4642_TF-IDF Questions/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS4642_TF_IDF_Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
